27cd85b3a59afcbc3f014f9c00e44dcb400144d7
François Pluchino <francois.pluchino@sonatra.com>
2015-12-27 21:16:29 +0100

    Add services of templater and chain loaders

--- DependencyInjection/SonatraMailerExtension.php
+++ DependencyInjection/SonatraMailerExtension.php
@@ -32,5 +32,6 @@ class SonatraMailerExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('templater.xml');
     }
 }
