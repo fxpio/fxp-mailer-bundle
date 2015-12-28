f6d844cb38eb24a77df288a5fc4a4675af5d7ef7
François Pluchino <francois.pluchino@sonatra.com>
2015-12-28 19:39:09 +0100

    Add config templates loaders tests

---
+++ Tests/Loader/ConfigLayoutLoaderTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\MailerBundle\Tests\Loader;
+
+use Sonatra\Bundle\MailerBundle\Exception\UnknownLayoutException;
+use Sonatra\Bundle\MailerBundle\Loader\ConfigLayoutLoader;
+use Sonatra\Bundle\MailerBundle\Model\LayoutInterface;
+
+/**
+ * Tests for config layout loader.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class ConfigLayoutLoaderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLoad()
+    {
+        $template = array(
+            'name' => 'test',
+            'label' => 'Test',
+            'description' => 'Description of test',
+            'enabled' => true,
+            'body' => 'Content of layout with {{ twig_variable }}.',
+            'translations' => array(
+                array(
+                    'locale' => 'fr',
+                    'label' => 'Test fr',
+                    'description' => 'Description du test',
+                    'body' => 'Contenu du layout avec {{ twig_variable }}.',
+                ),
+            ),
+        );
+
+        $loader = new ConfigLayoutLoader(array($template));
+
+        $this->assertInstanceOf(LayoutInterface::class, $loader->load('test'));
+    }
+
+    public function testLoadUnknownTemplate()
+    {
+        $this->setExpectedException(UnknownLayoutException::class, 'The "test" layout template does not exist');
+
+        $loader = new ConfigLayoutLoader(array());
+
+        $loader->load('test');
+    }
+}
